490a79340843717db90f5741a6a30a35017c98f9
Aleksandar Milosevic <aleksandarmilosevic98@gmail.com>
2019-11-24 14:18:54 +0100

    dodat pokret skoka karaktera/nedovrseno

--- main.c
+++ main.c
@@ -2,10 +2,19 @@
 #include<stdlib.h>
 #include <GL/glut.h>
 
+#define TIMER_ID 0
+#define TIMER_INTERVAL 20
+#define velicina 0.1
+#define jump_limit 0.2
 
-static float velicina = 0.1;
+static int jump_up = 0;
+static int jump_down = 0;
 
-static void on_keyboard(unsigned char key, int x, int y);
+
+static float xc=velicina/2;
+static float yc=velicina+0.05;
+
+static void on_keyboard(unsigned char key,int x,int y);
 static void on_timer(int value);
 static void on_display(void);
 
@@ -20,6 +29,8 @@ int main(int argc, char** argv){
     glutCreateWindow(argv[0]);
 
     glutDisplayFunc(on_display);
+    glutKeyboardFunc(on_keyboard);
+
     glutMainLoop();
 
     return 0;
@@ -29,11 +40,22 @@ static void on_display(void){
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    //Ogranicavajuce linije sa strana
+
+    glLineWidth(3);
+    glColor3f(0,0,1);
+    glBegin(GL_LINES);
+        glVertex2f(-1+0.05,1);
+        glVertex2f(-1+0.05,-1);
+    glEnd();
+    glBegin(GL_LINES);
+        glVertex2f(1-0.05,1);
+        glVertex2f(1-0.05,-1);
+    glEnd();
 
     //Startna linija
 
     glColor3f(1,0,1);
-    glLineWidth(3);
     glBegin(GL_LINES);
         glVertex2f(-1,-1+0.05);
         glVertex2f(1,-1+0.05);
@@ -42,11 +64,49 @@ static void on_display(void){
     //Prototip igrackog karaktera
 
     glBegin(GL_POLYGON);
-        glVertex2f(-velicina/2,-1+velicina+0.05);
-        glVertex2f(-velicina/2,-1+0.05);
-        glVertex2f(velicina/2,-1+0.05);
-        glVertex2f(velicina/2,-1+velicina+0.05);
+        glVertex2f(-xc,-1+yc);
+        glVertex2f(-xc,-1+0.05);
+        glVertex2f(xc,-1+0.05);
+        glVertex2f(xc,-1+yc);
     glEnd();
     glutSwapBuffers();
+
+}
+
+static void on_keyboard(unsigned char key, int x, int y){
+
+        switch (key)
+        {
+        case 27:
+            exit(0);
+
+        case ' ':
+            if(!jump_up) 
+                jump_up = 1;
+                glutTimerFunc(TIMER_INTERVAL,on_timer,TIMER_ID);
+            break;    
+        
+        default:
+            break;
+        }
+}
+
+//prototip funkcije skakanja
+
+static void on_timer(int value){
+
+    if(value!=TIMER_ID){
+        return;
+    }    
+    float dy = 0.003;
+    
+    if(jump_up==1){
+        yc+=dy;
+    }
+    glutPostRedisplay();
+
+    if(jump_up==1){
+        glutTimerFunc(TIMER_INTERVAL,on_timer,TIMER_ID);
+    }
 
 }
