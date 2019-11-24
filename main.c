1a4aa83ebbecf01a4ca5a8f09a71249cd46938f7
Aleksandar Milosevic <aleksandarmilosevic98@gmail.com>
2019-11-24 13:37:58 +0100

    Dodat probni prikaz igraca + postavljena pocetna scena

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #include<stdlib.h>
 #include <GL/glut.h>
 
+
+static float velicina = 0.1;
+
+static void on_keyboard(unsigned char key, int x, int y);
+static void on_timer(int value);
+static void on_display(void);
+
 int main(int argc, char** argv){
 
     
@@ -11,5 +18,35 @@ int main(int argc, char** argv){
     glutInitWindowSize(480, 854);
     glutInitWindowPosition(100, 100);
     glutCreateWindow(argv[0]);
+
+    glutDisplayFunc(on_display);
+    glutMainLoop();
+
+    return 0;
+}
+
+static void on_display(void){
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+    //Startna linija
+
+    glColor3f(1,0,1);
+    glLineWidth(3);
+    glBegin(GL_LINES);
+        glVertex2f(-1,-1+0.05);
+        glVertex2f(1,-1+0.05);
+    glEnd();
+
+    //Prototip igrackog karaktera
+
+    glBegin(GL_POLYGON);
+        glVertex2f(-velicina/2,-1+velicina+0.05);
+        glVertex2f(-velicina/2,-1+0.05);
+        glVertex2f(velicina/2,-1+0.05);
+        glVertex2f(velicina/2,-1+velicina+0.05);
+    glEnd();
+    glutSwapBuffers();
 
 }
